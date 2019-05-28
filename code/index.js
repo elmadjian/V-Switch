fedb2342eb0a2c3c4bd0c9ecc1ca939cecbf3e86
Cadu <cadu_elmadjian@yahoo.com.br>
2019-05-27 23:03:08 -0300

    adding electron interface

--- code/index.js
+++ code/index.js
@@ -1,8 +1,18 @@
 const {app, BrowserWindow} = require('electron');
 
 function createWindow() {
+    const {spawn} = require('child_process');
+    const backend = spawn('python', ['main.py']);
+
     window = new BrowserWindow({width: 800, height: 600});
-    window.loadFile('index.html')
+    window.loadFile('index.html');
+
+    var zerorpc = require('zerorpc');
+    var client = new zerorpc.Client();
+    client.connect("tcp://127.0.0.1:4242");
+    client.invoke("printSomething", function(error, res, more) {
+        console.log(res);
+    });
 }
 
 app.on('ready', createWindow);
--- code/main.py
+++ code/main.py
@@ -1,75 +1,22 @@
 import sys
-from PySide2.QtGui import QGuiApplication
-from PySide2.QtQml import QQmlApplicationEngine, qmlRegisterType
-from PySide2.QtCore import QUrl, Property, Signal
-import camera_old as camera
 import videoio
 import cv2
-import time
-from threading import Condition, Thread
+import zerorpc
 import numpy as np
 
-global scene_cam
-global le_cam
-
-def start(source, cam):
-    cap = cv2.VideoCapture(source)
-    while not cap.isOpened() and source < 10:
-        source += 1
-        time.sleep(0.2)
-        cap = cv2.VideoCapture(source)
-    while True:
-        ret, frame = cap.read()
-        if ret:
-            #PROCESSING STUFF
-            cam.set_image(frame)     
-    cap.release()
-
+class Test(object):
+    def printSomething(self):
+        return "Estou dando ola do Python!"
 
 if __name__=='__main__':
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
 
     videoio = videoio.VideoIO()
     cameras = videoio.get_cameras()
 
-    # scene_cam = camera.Camera(0)
-    # le_cam    = camera.Camera2(1)
-
-    # scthread = Thread(target=start, args=(0, scene_cam,))
-    # lethread = Thread(target=start, args=(1, le_cam,))
-
-    # scthread.start()
-    # lethread.start()
-
-    # img = cv2.imread("../UI/test.jpg")
-    # img2 = cv2.imread("../UI/test2.jpg")
-
-    # scene_cam.set_image(img)
-    # le_cam.set_image(img2)
-    #imageChanged = Signal(np.ndarray)
-    #scene_image = Property(np.ndarray, scene_cam.get_image, scene_cam.set_image, notify=imageChanged)
-    #left_eye_cam = camera.QMLLeftEyeCamera(1)
+    addr = 'tcp://127.0.0.1:4242'
+    s = zerorpc.Server(Test())
+    s.bind(addr)
+    print("running server at", addr)
+    s.run()
 
 
-    
-
-
-    
-    # img = cv2.imread("../UI/test.jpg")
-
-
-    qmlRegisterType(camera.QMLCamera, "CVStuff", 1, 0, "SceneCamera")
-    qmlRegisterType(camera.QMLCamera2, "CVStuff", 1, 0, "LeftEyeCamera")
-    #qmlRegisterType(camera.QMLCamera, "CVStuff", 1, 0, "RightEyeCamera")
-
-    # engine.rootContext().setContextProperty("sceneCamCV", scene_cam)
-    # engine.rootContext().setContextProperty("leftEyeCamCV", le_cam)
-    engine.rootContext().setContextProperty("cameraSources", cameras)
-    engine.load(QUrl("../UI/v_switch/main.qml"))
-
-
-    if not engine.rootObjects():
-        sys.exit(-1)
-
-    sys.exit(app.exec_())
